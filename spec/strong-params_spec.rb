7d1ef7c1cd154e85ebef8820c84308f4e733df52
Yukihiro <suruseas@gmail.com>
2017-01-30 20:37:30 +0900

    Add nested parameters test.

--- spec/strong-params_spec.rb
+++ spec/strong-params_spec.rb
@@ -8,33 +8,68 @@ describe Sinatra::StrongParams do
 
   context "using allows filter" do
 
-    let(:request_params) { { id: 'id', action: 'action', not_allows: 'not_allows' } }
+    context "with no nested params" do
 
-    it 'supports accessing params with string keys' do
-      actual_params = nil
-      mock_app do
-        register Sinatra::StrongParams
-        get '/', allows: [:id, :action] { actual_params = params }
+      let(:request_params) { { id: 'id', action: 'action', not_allows: 'not_allows' } }
+
+      it 'supports accessing params with string keys' do
+        actual_params = nil
+        mock_app do
+          register Sinatra::StrongParams
+          get '/', allows: [:id, :action] { actual_params = params }
+        end
+
+        get '/', request_params
+        expect(actual_params['id']).to eq request_params[:id]
+        expect(actual_params['action']).to eq request_params[:action]
+        expect(actual_params['not_allows']).to eq nil
       end
 
-      get '/', request_params
-      expect(actual_params['id']).to eq request_params[:id]
-      expect(actual_params['action']).to eq request_params[:action]
-      expect(actual_params['not_allows']).to eq nil
-    end
+      it 'supports accessing params with symbol keys' do
+        actual_params = nil
+        mock_app do
+          register Sinatra::StrongParams
+          get '/', allows: [:id, :action] { actual_params = params }
+        end
 
-    it 'supports accessing params with symbol keys' do
-      actual_params = nil
-      mock_app do
-        register Sinatra::StrongParams
-        get '/', allows: [:id, :action] { actual_params = params }
+        get '/', request_params
+        expect(actual_params[:id]).to eq request_params[:id]
+        expect(actual_params[:action]).to eq request_params[:action]
+        expect(actual_params[:not_allows]).to eq nil
       end
 
-      get '/', request_params
-      expect(actual_params[:id]).to eq request_params[:id]
-      expect(actual_params[:action]).to eq request_params[:action]
-      expect(actual_params[:not_allows]).to eq nil
     end
+
+    context "with nested params" do
+
+      let(:request_params) { { id: [ { in_array: 'in_array'} ], action: { nested_hash: 'nested_hash'} }}
+
+      it 'supports accessing params with string keys' do
+        actual_params = nil
+        mock_app do
+          register Sinatra::StrongParams
+          get '/', allows: [:id, :action] { actual_params = params }
+        end
+
+        get '/', request_params
+        expect(actual_params['id'][0]['in_array']).to eq request_params[:id][0][:in_array]
+        expect(actual_params['action']['nested_hash']).to eq request_params[:action][:nested_hash]
+      end
+
+      it 'supports accessing params with symbol keys' do
+        actual_params = nil
+        mock_app do
+          register Sinatra::StrongParams
+          get '/', allows: [:id, :action] { actual_params = params }
+        end
+
+        get '/', request_params
+        expect(actual_params[:id][0][:in_array]).to eq request_params[:id][0][:in_array]
+        expect(actual_params[:action][:nested_hash]).to eq request_params[:action][:nested_hash]
+      end
+
+    end
+
   end
 
   context "using needs filter" do
